195af1d0639d861d04d28fff87de2f7440e057c0
stasrusnak <stas_rusnak@gmail.com>
2024-05-22 22:06:00 +0300

    add locations

--- Locales.lua
+++ Locales.lua
@@ -1,120 +1,38 @@
-
 -- English
-local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "enUS", true);
+local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "enUS", true)
 
-if not L then return end
-
-L["Automatic"] = true;
-L["Choose the orientation of the raid markers bar."] = true;
-L["Click to clear the mark."] = true;
-L["Click to mark the target."] = true;
-L["Raid Markers Bar"] = true;
-L["Raid Markers"] = true;
-L["Select how the raid markers bar will be displayed."] = true;
-L["The direction that the mark frames will grow from the anchor."] = true;
-
+if not L then return end 
+	L["Automatic"] = true
+	L["Visibility"] = true
+	L["Select how the dungeon difficulty bar will be displayed."] = true
+	L["Hide"] = true
+	L["Show"] = true
+	L["Dungeon Difficulty Bar"] = true
+	L["Button Size"] = true
+	L["The size of the action button."] = true
+	L["Text Size"] = true
+	L["The size of the text."] = true
+	L["Text Offset X"] = true
+	L["The horizontal offset of the text."] = true
+	L["Text Offset Y"] = true
+	L["The vertical offset of the text."] = true 
 if GetLocale() == "enUS" then return end
 
 -- Russian
 local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "ruRU")
 if L then
-	L["Automatic"] = "Автоматически"
-	L["Choose the orientation of the raid markers bar."] = "Выберите ориентацию панели меток";
-	L["Click to clear the mark."] = "Снять метку";
-	L["Click to mark the target."] = "Пометить цель";
-	L["Raid Markers Bar"] = "Сложность";
-	L["Raid Markers"] = "Сложность";
-	L["Select how the raid markers bar will be displayed."] = "Выберите, как будет отображаться панель меток.";
-	L["The direction that the mark frames will grow from the anchor."] = "Направление, в котором будут расположены кнопки меток относительно фиксатора.";
-end
-
--- German
-local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "deDE")
-if L then
-	L["Automatic"] = "Automatisch"
-	L["Choose the orientation of the raid markers bar."] = true;
-	L["Click to clear the mark."] = true;
-	L["Click to mark the target."] = true;
-	L["Raid Markers Bar"] = true;
-	L["Raid Markers"] = true;
-	L["Select how the raid markers bar will be displayed."] = true;
-	L["The direction that the mark frames will grow from the anchor."] = true;
-end
-
--- Spanish
-local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "esES")
-if L then
-	L["Automatic"] = true;
-	L["Choose the orientation of the raid markers bar."] = true;
-	L["Click to clear the mark."] = true;
-	L["Click to mark the target."] = true;
-	L["Raid Markers Bar"] = true;
-	L["Raid Markers"] = true;
-	L["Select how the raid markers bar will be displayed."] = true;
-	L["The direction that the mark frames will grow from the anchor."] = true;
-end
-
--- French
-local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "frFR")
-if L then
-	L["Automatic"] = "Auto"
-	L["Choose the orientation of the raid markers bar."] = true;
-	L["Click to clear the mark."] = true;
-	L["Click to mark the target."] = true;
-	L["Raid Markers Bar"] = true;
-	L["Raid Markers"] = true;
-	L["Select how the raid markers bar will be displayed."] = true;
-	L["The direction that the mark frames will grow from the anchor."] = true;
-end
-
--- Korean
-local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "koKR")
-if L then
-	L["Automatic"] = "자동"
-	L["Choose the orientation of the raid markers bar."] = true;
-	L["Click to clear the mark."] = true;
-	L["Click to mark the target."] = true;
-	L["Raid Markers Bar"] = true;
-	L["Raid Markers"] = true;
-	L["Select how the raid markers bar will be displayed."] = true;
-	L["The direction that the mark frames will grow from the anchor."] = true;
-end
-
--- Portuguese
-local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "ptBR")
-if L then
-	L["Automatic"] = true;
-	L["Choose the orientation of the raid markers bar."] = true;
-	L["Click to clear the mark."] = true;
-	L["Click to mark the target."] = true;
-	L["Raid Markers Bar"] = true;
-	L["Raid Markers"] = true;
-	L["Select how the raid markers bar will be displayed."] = true;
-	L["The direction that the mark frames will grow from the anchor."] = true;
-end
-
---Chinese (China, simplified)
-local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "zhCN")
-if L then
-	L["Automatic"] = "自动"
-	L["Choose the orientation of the raid markers bar."] = true;
-	L["Click to clear the mark."] = true;
-	L["Click to mark the target."] = true;
-	L["Raid Markers Bar"] = true;
-	L["Raid Markers"] = true;
-	L["Select how the raid markers bar will be displayed."] = true;
-	L["The direction that the mark frames will grow from the anchor."] = true;
-end
-
---Chinese (Taiwan, traditional)
-local L = LibStub("AceLocale-3.0"):NewLocale("ElvUI", "zhTW")
-if L then
-	L["Automatic"] = "自動"
-	L["Choose the orientation of the raid markers bar."] = true;
-	L["Click to clear the mark."] = true;
-	L["Click to mark the target."] = true;
-	L["Raid Markers Bar"] = true;
-	L["Raid Markers"] = true;
-	L["Select how the raid markers bar will be displayed."] = true;
-	L["The direction that the mark frames will grow from the anchor."] = true;
+    L["Automatic"] = "Автоматически"
+    L["Visibility"] = "Видимость"
+    L["Select how the dungeon difficulty bar will be displayed."] = "Выберите, как будет отображаться панель сложности подземелья."
+    L["Hide"] = "Скрыть"
+    L["Show"] = "Показать"
+    L["Dungeon Difficulty Bar"] = "Панель Dungeon Difficulty"
+    L["Button Size"] = "Размер кнопки"
+    L["The size of the action button."] = "Размер кнопки действия."
+    L["Text Size"] = "Размер текста"
+    L["The size of the text."] = "Размер текста."
+    L["Text Offset X"] = "Смещение текста по X"
+    L["The horizontal offset of the text."] = "Горизонтальное смещение текста."
+    L["Text Offset Y"] = "Смещение текста по Y"
+    L["The vertical offset of the text."] = "Вертикальное смещение текста."
 end
